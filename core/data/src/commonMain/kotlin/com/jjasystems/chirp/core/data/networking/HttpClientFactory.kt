77a035c5068e010e3e423e010e189ee92454ba86
Juan Jose Abuin <juanj.abuin@gmail.com>
2025-10-01 09:25:57 -0300

    6.27 Refresh token mechanism

--- core/data/src/commonMain/kotlin/com/jjasystems/chirp/core/data/networking/HttpClientFactory.kt
+++ core/data/src/commonMain/kotlin/com/jjasystems/chirp/core/data/networking/HttpClientFactory.kt
@@ -1,22 +1,34 @@
 package com.jjasystems.chirp.core.data.networking
 
+import com.jjasystems.chirp.core.data.dto.AuthInfoSerializable
+import com.jjasystems.chirp.core.data.dto.request.RefreshRequest
+import com.jjasystems.chirp.core.data.mapper.toDomain
+import com.jjasystems.chirp.core.domain.auth.SessionStorage
 import com.jjasystems.chirp.core.domain.logging.ChirpLogger
+import com.jjasystems.chirp.core.domain.util.onFailure
+import com.jjasystems.chirp.core.domain.util.onSuccess
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.client.statement.request
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.serialization.json.Json
 
 class HttpClientFactory(
-    private val chirpLogger: ChirpLogger
+    private val chirpLogger: ChirpLogger,
+    private val sessionStorage: SessionStorage
 ) {
 
     fun create(engine: HttpClientEngine): HttpClient {
@@ -48,8 +60,59 @@ class HttpClientFactory(
             }
 
             defaultRequest {
-                contentType(ContentType.Application.Json)
+                contentType(ContentType.Application.Json
+            }
 
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        sessionStorage
+                            .observeAuthInfo()
+                            .firstOrNull()
+                            ?.let {
+                                BearerTokens(
+                                    it.accessToken,
+                                    it.refreshToken
+                                )
+                            }
+                    }
+                    refreshTokens {
+                        if (response.request.url.encodedPath.contains("auth/")) {
+                            return@refreshTokens
+                        }
+
+                        val authInfo = sessionStorage
+                            .observeAuthInfo()
+                            .firstOrNull()
+
+                        if (authInfo?.refreshToken.isNullOrBlank()) {
+                            sessionStorage.set(null)
+                            return@refreshTokens null
+                        }
+
+                        var bearerTokens: BearerTokens? = null
+
+                        client.post<RefreshRequest, AuthInfoSerializable>(
+                            route = "/auth/refresh",
+                            body = RefreshRequest(
+                                refreshToken = authInfo.refreshToken
+                            ),
+                            builder = {
+                                markAsRefreshTokenRequest()
+                            }
+                        ).onSuccess { newAuthInfo ->
+                            sessionStorage.set(newAuthInfo.toDomain())
+                            bearerTokens = BearerTokens(
+                                newAuthInfo.accessToken,
+                                newAuthInfo.refreshToken
+                            )
+                        }.onFailure { error ->
+                            sessionStorage.set(null)
+                        }
+
+                        bearerTokens
+                    }
+                }
             }
         }
     }
